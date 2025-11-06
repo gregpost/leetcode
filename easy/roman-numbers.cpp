2301b2f198378476bb23e5541a3580e38c186ee4
gregpost <gpostolskiy@gmail.com>
2025-11-06 08:48:16 +0300

    New task

---
+++ easy/roman-numbers.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+	int romanToInt(string s) {
+		unordered_map<char, int> um1 = {
+			'I': 1,
+			'V': 5,
+			'X': 10,
+			'L': 50,
+			'C': 100,
+			'D': 500,
+			'M': 1000
+		};
+		unordered_map<char, int> um2 = {
+			'V': 3,
+			'X': 8,
+			'L': 30,
+			'C': 80,
+			'D': 30,
+			'M': 800
+		};
+		
+		const int n = string.size();
+		int i = 0;
+		bool b = false;
+		int pv = 0;
+		int sum = 0;
+		while (i < n) {
+			if (pv < s[i]) b = true;
+			pv = s[i];
+			if (b) s += um2[pv];
+			else s += um1[pv];
+			i++;
+		}
+	}
+};
